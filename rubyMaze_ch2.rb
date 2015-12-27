e604c05adf7b350cf5ff9a3c23f4d7d11cc4e30e
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-26 20:33:51 -0500

    Updating to add sidewinder algorthim

--- rubyMaze_ch2.rb
+++ rubyMaze_ch2.rb
@@ -1,6 +1,7 @@
 require './classes/grid'
 require './classes/binary_tree'
+require './classes/sidewinder'
 
-grid = Grid.new(4,4);
-BinaryTree.new;
+grid = Grid.new(10,10);
+Sidewinder.on(grid);
 puts grid
